34cf5e91f7cc87c5fd56e22280efd58389a9a5d0
krouzek <krouzek@pcfeib207-22-67.vsb.cz>
2025-05-15 16:57:20 +0200

    kapka - hra - 15.5.2025

--- core/src/main/java/io/github/hra/Kapka.java
+++ core/src/main/java/io/github/hra/Kapka.java
@@ -1,6 +1,40 @@
 package io.github.hra;
 
+import java.lang.reflect.Method;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
 public class Kapka {
-  private float speed;
-  private Texture dropTexture;
+	private float speed;
+	private Texture dropTexture;
+
+	public Kapka(float speed, Texture dropTexture) {
+		super();
+		this.speed = speed;
+		this.dropTexture = dropTexture;
+	}
+
+	public void draw(SpriteBatch batch) {
+		ScreenUtils.clear(Color.BLACK);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+        
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+        
+        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
+        bucketSprite.draw(spriteBatch); // Sprites have their own draw method
+        
+     // draw each sprite
+        for (Sprite dropSprite : dropSprites) {
+            dropSprite.draw(spriteBatch);
+        }
+        
+        spriteBatch.end();
+	}
 }
